2180f45b2b500aedf9b04044368f4d94ce5d6a64
Hilman (Imen) <141418226+Hilmanalmas@users.noreply.github.com>
2026-03-08 01:03:25 +0700

    bismillah

--- wp-config.php
+++ wp-config.php
@@ -143,7 +143,10 @@ if ( isset( $_SERVER['HTTP_HOST'] ) ) {
 }
 
 // Tambahkan cookie domain untuk mencegah masalah redirect auth
-define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
+// Jangan definisikan COOKIE_DOMAIN jika localhost/127.0.0.1 karena WordPress sering mem-blokirnya
+if (!in_array($_SERVER['HTTP_HOST'], ['localhost', '127.0.0.1', 'localhost:8000', '127.0.0.1:8000'])) {
+    define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
+}
 define('WP_HOME_OVERRIDE', true);
 
 /* That's all, stop editing! Happy publishing. */
